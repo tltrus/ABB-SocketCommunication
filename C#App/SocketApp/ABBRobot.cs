931aadb00676f2c40a33ec79f32434078126e66e
tltrus <77125487+tltrus@users.noreply.github.com>
2024-04-30 23:15:50 +0400

    up

---
+++ C#App/SocketApp/ABBRobot.cs
@@ -0,0 +1,76 @@
+﻿using ABB.Robotics.Controllers;
+using ABB.Robotics.Controllers.RapidDomain;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Windows;
+
+namespace Painting
+{
+    internal class ABBRobot
+    {
+        public Controller SelectedController { get; set;  }
+        private TcpClient TcpClient;
+        private NetworkStream ClientStream;
+        private int PORT = 4000;
+        private string IP;
+
+        public delegate void MessageHandler(string message);
+        public event MessageHandler SendMessageEvent;
+        public event MessageHandler GetMessageEvent;
+
+        public ABBRobot()
+        {
+
+        }
+
+        public void Connect(ControllerInfo controllerInfo)
+        {
+            if (controllerInfo is null) return;
+
+            SelectedController = Controller.Connect(controllerInfo, ConnectionType.Standalone);
+            SelectedController.Logon(UserInfo.DefaultUser);
+
+            IP = SelectedController.IPAddress.ToString();
+        }
+
+        public void SendMessage(string message)
+        {
+            if (ClientStream is null) return;
+
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(message);
+            ClientStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+            SendMessageEvent.Invoke(message); // Event
+
+            ReceiveMessage();
+        }
+
+        private void ReceiveMessage()
+        {
+            if (ClientStream is null) return;
+
+            byte[] bytesToRead = new byte[TcpClient.ReceiveBufferSize];
+            int bytesRead = ClientStream.Read(bytesToRead, 0, TcpClient.ReceiveBufferSize);
+            string ReceivedMessage = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+
+            GetMessageEvent.Invoke(ReceivedMessage); // Event
+        }
+
+        public void OpenSocket()
+        {
+            TcpClient = new TcpClient(IP, PORT);
+            ClientStream = TcpClient.GetStream();
+        }
+        public void CloseSocket()
+        {
+            if (TcpClient is null) return;
+            
+            TcpClient.Close();
+        }
+    }
+}
---
+++ C#App/SocketApp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using ABB.Robotics.Controllers.Discovery;
+using ABB.Robotics.Controllers;
+
+
+namespace Painting
+{
+    public partial class MainWindow : Window
+    {
+        ABBRobot Robot;
+        NetworkScanner Netscaner { get; set; }
+        ControllerInfoCollection Controllers { get; set; }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            Robot = new ABBRobot();
+            NetScan();
+
+            Robot.SendMessageEvent += (msg) => rtbLog.AppendText(String.Format("\rC# : {0}", msg)); 
+            Robot.GetMessageEvent += (msg) => rtbLog.AppendText(String.Format("\rABB: {0}", msg));
+        }
+
+        private void NetScan()
+        {
+            Netscaner = new NetworkScanner();
+            Netscaner.Scan();
+            Controllers = Netscaner.Controllers;
+
+            foreach (ControllerInfo c in Controllers)
+            {
+                cbox_Controllers.Items.Add(c);
+            }
+        }
+
+        private void cbox_Controllers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                var comboBoxControllers = sender as ComboBox;
+                Robot.Connect((ControllerInfo)comboBoxControllers?.SelectedItem);
+
+                btnSend.IsEnabled = true;
+
+                Robot.OpenSocket();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnSend_Click(object sender, RoutedEventArgs e) => Robot.SendMessage(tbSend.Text);
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) => Robot.CloseSocket();
+
+    }
+}
